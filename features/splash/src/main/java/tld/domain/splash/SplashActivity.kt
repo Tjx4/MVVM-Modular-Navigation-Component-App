1e5796cc26d3fa228f4d4418d180d21f27bb5abd
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2021-12-21 14:24:46 +0200

    Added splash

--- features/splash/src/main/java/tld/domain/splash/SplashActivity.kt
+++ features/splash/src/main/java/tld/domain/splash/SplashActivity.kt
@@ -6,5 +6,6 @@ import android.os.Bundle
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        actionBar?.title = "DD"
     }
 }
