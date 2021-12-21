618151bb0e13f9bf7c7e37487b7a91a010d9076a
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2021-12-21 14:21:46 +0200

    Added splash

---
+++ features/splash/src/main/java/tld/domain/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package tld.domain.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
