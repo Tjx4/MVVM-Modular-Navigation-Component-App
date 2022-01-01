9a00185ff8b0be1a6148520e3aba6cb6ac3eff40
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2022-01-01 15:50:54 +0200

    added error message

--- repositories/src/main/java/com/domain/repositories/authentication/AuthenticationRepositoryImpl.kt
+++ repositories/src/main/java/com/domain/repositories/authentication/AuthenticationRepositoryImpl.kt
@@ -1,10 +1,11 @@
 package com.domain.repositories.authentication
 
 import com.domain.core.persistance.room.ForexDB
+import com.domain.myapplication.models.ErrorResponse
 import com.domain.myapplication.models.LoginResponse
 
 class AuthenticationRepositoryImpl(private val database: ForexDB) : AuthenticationRepository {
     override fun loginUser(username: String, password: String): LoginResponse? {
-        return null
+        return LoginResponse(null, ErrorResponse("Incorrect password"))
     }
 }
