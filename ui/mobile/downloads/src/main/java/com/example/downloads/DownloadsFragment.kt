e55184280f0e1d9f35fa72110f2bf6460146d5c9
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2022-01-05 21:37:31 +0200

    Added download flow

--- ui/mobile/downloads/src/main/java/com/example/downloads/DownloadsFragment.kt
+++ ui/mobile/downloads/src/main/java/com/example/downloads/DownloadsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.domain.myapplication.base.BaseFragment
 import com.example.downloads.databinding.FragmentDownloadsBinding
+import kotlinx.android.synthetic.main.fragment_downloads.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DownloadsFragment : BaseFragment() {
@@ -23,6 +25,20 @@ class DownloadsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addObservers()
+    }
 
+    private fun addObservers() {
+        downloadsViewModel.count.observe(viewLifecycleOwner, { onCountSet(it) })
+        downloadsViewModel.isComplete.observe(viewLifecycleOwner, { onComplete() })
+    }
+
+    fun onCountSet(count: String){
+
+    }
+
+    fun onComplete(){
+        tvText.visibility = View.GONE
+        Toast.makeText(context, "Download complete", Toast.LENGTH_SHORT).show()
     }
 }
--- ui/mobile/downloads/src/main/java/com/example/downloads/DownloadsViewModel.kt
+++ ui/mobile/downloads/src/main/java/com/example/downloads/DownloadsViewModel.kt
@@ -2,8 +2,40 @@ package com.example.downloads
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class DownloadsViewModel(private val app: Application) : AndroidViewModel(app){
 
+    private val _count: MutableLiveData<String> = MutableLiveData()
+    val count: MutableLiveData<String>
+        get() = _count
+
+    private val _isComplete: MutableLiveData<Boolean> = MutableLiveData()
+    val isComplete: MutableLiveData<Boolean>
+        get() = _isComplete
+
+    init {
+        val flow = flow<Int> {
+            for (i in 10 downTo 0){
+                emit(i)
+                delay(1000)
+            }
+        }
+
+        viewModelScope.launch (Dispatchers.Main) {
+            flow.collect {
+                when(it){
+                    0 -> _isComplete.value = true
+                    else -> _count.value = "$it"
+                }
+            }
+        }
+    }
 
 }
