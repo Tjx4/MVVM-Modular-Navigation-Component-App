b59fe5a1022e6f59446fcaf03cc9c31a76021e52
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2022-04-10 16:41:30 +0200

    added mock flavour to core

--- core/src/main/java/com/domain/core/networking/retrofit/Environments.kt
+++ core/src/main/java/com/domain/core/networking/retrofit/Environments.kt
@@ -1,6 +1,8 @@
 package com.domain.core.networking.retrofit
 
-enum class Environments(var url: String) {
-    Production("http://appicsoftware.xyz/ "),
-    UAT("http://appicsoftware.xyz/")
+enum class Environments(var url: String, var ip: String) {
+    Production("http://appicsoftware.xyz/", "160.153.129.204"),
+    Staging("http://appicsoftware.xyz/", ""),
+    Dev("http://appicsoftware.xyz/", ""),
+    Mock("", "")
 }
