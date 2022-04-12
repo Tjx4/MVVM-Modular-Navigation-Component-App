d79533ef5e29c477c744591acaf886febeebba27
Tshepo Mahlaula <tshepo.mahlaula@multichoice.com>
2022-04-12 10:08:06 +0200

    Added video deep link

--- core/src/staging/java/com/domain/core/networking/retrofit/API.kt
+++ core/src/staging/java/com/domain/core/networking/retrofit/API.kt
@@ -3,6 +3,7 @@ package com.domain.core.networking.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/*
 object API {
     var retrofit: RetrofitServices
 
@@ -16,4 +17,4 @@ object API {
         API.retrofit = retrofit.create(RetrofitServices::class.java)
     }
 }
-
+*/
